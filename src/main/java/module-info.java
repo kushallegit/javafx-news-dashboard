c0e079a85ff6e42881c7e7585a590c86574bef7c
Kushal Gurung <169062369+kushallegit@users.noreply.github.com>
2025-04-04 12:28:58 -0400

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.sem3.java.assignment2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.sem3.java.assignment2 to javafx.fxml;
-    exports com.sem3.java.assignment2;
-}
